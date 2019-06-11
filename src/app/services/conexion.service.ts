929180f75241d587a1be134acef8bec188029400
cristianfchq <cristian.fchq@gmail.com>
2019-06-11 15:10:06 -0400

    mostramos id de cada elemento y colocamos algunos disenios de bootstrap

--- src/app/services/conexion.service.ts
+++ src/app/services/conexion.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
+import { map } from 'rxjs/operators';
+
 export interface Item { name: string; }
 
 @Injectable({
@@ -13,7 +15,13 @@ export class ConexionService {
   items: Observable<Item[]>;
   constructor(private afs: AngularFirestore) {
     this.itemsCollection = afs.collection<Item>('items');
-    this.items = this.itemsCollection.valueChanges();
+    this.items = this.itemsCollection.snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as Item;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }))
+    );
    }
 
    ListaItem(){
